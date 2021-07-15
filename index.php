72a0eed10a5b2e4ead6b600457cb31eba9e557b5
harbindsingh <60515499+harbindsingh@users.noreply.github.com>
2021-07-15 15:42:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php  
-echo ("Hello World");
-?>
+<?php
+
+$con=mysqli_connect("shareddb-y.hosting.stackcp.net","emailverify-ad80","emailverify@");
+if(!$con)
+	echo "Not connected to server";
+
+if(!mysqli_select_db($con,'email_OTP-313539265d')) 
+	echo "Database not selected";
+	
+	?>
